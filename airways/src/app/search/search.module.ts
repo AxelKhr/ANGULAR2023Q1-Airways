2f5dfbd82753cc98e79f7bf83da76e0b81676409
Aleksei <toporov.aleksei@gmail.com>
2023-05-01 14:13:04 +0300

    feat: search form

--- airways/src/app/search/search.module.ts
+++ airways/src/app/search/search.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { SearchRoutingModule } from './search-routing.module';
+import { BindAutocompletesPipe } from './pipe/bind-autocompletes.pipe';
 
 @NgModule({
-  declarations: [
-    SearchPageComponent,
-  ],
+  declarations: [SearchPageComponent, BindAutocompletesPipe],
   imports: [
     CommonModule,
     SearchRoutingModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    CdkMenuModule,
+    MatButtonModule,
   ],
 })
-export class SearchModule { }
+export class SearchModule {}
