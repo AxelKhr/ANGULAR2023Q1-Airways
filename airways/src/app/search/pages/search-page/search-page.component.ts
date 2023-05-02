fbb1af67abe4d9b5c275d424e19b5d0a02e1149c
Aliaksandr Khramau <axel.khr@gmail.com>
2023-05-02 20:17:19 +0200

    refactor: remove tests elements

--- airways/src/app/search/pages/search-page/search-page.component.ts
+++ airways/src/app/search/pages/search-page/search-page.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import * as Actions from 'src/app/redux/actions';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.scss'],
 })
-export class SearchPageComponent {
-  private isFlag = false;
-
-  constructor(private store: Store) {}
-
-  onClick() {
-    this.isFlag = !this.isFlag;
-    this.store.dispatch(Actions.general.setIsMainStyle({ isMainStyle: this.isFlag }));
-  }
-}
+export class SearchPageComponent { }
