965503baa60a76781936d684e1f08e258748b238
Aliaksandr Khramau <axel.khr@gmail.com>
2023-05-08 17:36:29 +0200

    feat: fix disable  background

--- airways/src/app/app.component.ts
+++ airways/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as Selectors from 'src/app/redux/selectors';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'airways';
+
+  isBackgroundVisible$: Observable<boolean> = this.store.select(
+    (Selectors.general.selectIsMainStyle),
+  );
+
+  constructor(private store: Store) { }
 }
--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -20,6 +21,7 @@ import { effects } from './redux/effects';
     AppComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     CoreModule,
