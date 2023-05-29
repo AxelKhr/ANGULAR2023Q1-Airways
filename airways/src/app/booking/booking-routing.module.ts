f97b621b4323f013addbc305d11b3debb6390956
Aliaksandr Khramau <axel.khr@gmail.com>
2023-05-29 12:28:44 +0200

    feat: add authorization guard for some pages

--- airways/src/app/booking/booking-routing.module.ts
+++ airways/src/app/booking/booking-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/guards/auth.guard';
 import { BookingPageComponent } from './pages/booking-page/booking-page.component';
 import { ProcessPageComponent } from './pages/process-page/process-page.component';
 import { SelectPageComponent } from './pages/select-page/select-page.component';
@@ -11,8 +12,8 @@ const routes: Routes = [
     component: BookingPageComponent,
     children: [
       { path: 'select', component: SelectPageComponent },
-      { path: 'process', component: ProcessPageComponent },
-      { path: 'summary', component: SummaryPageComponent },
+      { path: 'process', component: ProcessPageComponent, canActivate: [AuthGuard] },
+      { path: 'summary', component: SummaryPageComponent, canActivate: [AuthGuard] },
       { path: '', redirectTo: 'select', pathMatch: 'full' },
     ],
   },
