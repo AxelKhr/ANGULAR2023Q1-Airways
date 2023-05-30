0a1a7c595b5988ef60d4dad00a8074fe19435062
Aliaksandr Khramau <axel.khr@gmail.com>
2023-05-30 15:33:15 +0200

    refactor: change message for errors

--- airways/src/app/core/services/api.service.ts
+++ airways/src/app/core/services/api.service.ts
@@ -10,10 +10,6 @@ import { ICountryCodeModel } from 'src/app/shared/models/country-code.model';
 import { IAirportModel } from 'src/app/shared/models/airport.model';
 import { IFlightsRequestModel } from 'src/app/shared/models/flights-request.model';
 import { IFlightsResponseModel } from 'src/app/shared/models/flights-response.model';
-import { IUserProfileModel } from 'src/app/shared/models/user-profile.model';
-import { IUserLoginModel } from 'src/app/shared/models/user-login.model';
-import { IUserDataModel } from 'src/app/shared/models/user-data.model';
-import { IUserCheckModel } from 'src/app/shared/models/user-check.model';
 import { IOrderSaveModel } from 'src/app/shared/models/order-save.model';
 import * as Storage from 'src/app/core/storage/storage';
 import { ISavedOrderModel } from 'src/app/shared/models/order.model';
@@ -29,40 +25,7 @@ export class ApiService {
   }
 
   private handlerError(error: HttpErrorResponse) {
-    return throwError(() => error.error.message);
-  }
-
-  registrateUser(userProfile: IUserProfileModel) {
-    return this.http.post(
-      this.getApiUrl(API_DEF.API_URL_USER_REGISTRATION),
-      userProfile,
-    ).pipe(
-      retry(API_DEF.API_NUMBER_OF_REPEATS),
-      catchError(this.handlerError),
-    );
-  }
-
-  loginUser(userLogin: IUserLoginModel) {
-    return this.http.post<IUserDataModel>(
-      this.getApiUrl(API_DEF.API_URL_USER_LOGIN),
-      userLogin,
-    ).pipe(
-      retry(API_DEF.API_NUMBER_OF_REPEATS),
-      catchError(this.handlerError),
-    );
-  }
-
-  checkUser(userData: IUserCheckModel) {
-    return this.http.get<IUserProfileModel>(
-      this.getApiUrl(API_DEF.API_URL_USER_CHECK),
-      {
-        headers: new HttpHeaders().append('Authorization', `Bearer ${userData.token}`),
-        params: new HttpParams().append('id', userData.userId),
-      },
-    ).pipe(
-      retry(API_DEF.API_NUMBER_OF_REPEATS),
-      catchError(this.handlerError),
-    );
+    return throwError(() => error.message);
   }
 
   getCountryCodes() {
